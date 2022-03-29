bc16a7c25db8f6f3b53aec8dca9ec2b66ed0c1d8
Ana Gondin <anapaulagondinbastos@gmail.com>
2022-03-29 14:11:28 -0300

    exercicios java

---
+++ exemploSwitch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exemploSwitch {
+        public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o final da placa: ");
+        int finalPlaca = entrada.nextInt();
+
+        switch (finalPlaca) {
+            case 1:
+            System.out.println("Vencimento dia 11 de Janeiro.");
+            break;
+
+            case 2:
+            System.out.println("Vencimento dia 12 de Janeiro.");
+            break;
+            
+            case 3:
+            System.out.println("Vencimento dia 12 de Janeiro.");
+            break;
+
+            default:
+            System.out.println("Vencimento sem data, desculpe");
+            
+
+        }
+
+    }
+
+
+}
